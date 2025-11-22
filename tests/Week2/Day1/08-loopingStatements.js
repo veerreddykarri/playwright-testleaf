d25a5ae5a14f7a7c2f92c355753e6daeafd59486
Veer Karri <veerreddykarri@gmail.com>
2025-11-22 17:48:41 +0000

    Week 2 Day1 Tasks

---
+++ tests/Week2/Day1/08-loopingStatements.js
@@ -0,0 +1,11 @@
+/* Write a JavaScript function named `printOddNumbers` that prints odd numbers between 1 and 25 */
+
+function printOddNumbers() {
+    for (let i = 1; i <= 25; i++) {
+        if (i % 2 !== 0) {
+            console.log(i);
+        }
+    }
+}
+
+printOddNumbers();
